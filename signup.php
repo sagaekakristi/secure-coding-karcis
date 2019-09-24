96c42482bed58a7d7ebc1b278570592f50161125
develop <ikhlas.firlana@gmail.com>
2019-09-24 09:11:57 +0700

    recaptcha v3

--- signup.php
+++ signup.php
@@ -1,9 +1,32 @@
 <?php
 include "header.php";
 ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://www.google.com/recaptcha/api.js?render=6Le5A7oUAAAAANabUaFxH5AD_qwB3WVWnkI4yOyo"></script>
+<script>
+function onClickSubmit() {
 
+    var fullname = $('#fullname').val();
+    var email = $('#email').val();
+    var password = $("#password").val();
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6Le5A7oUAAAAANabUaFxH5AD_qwB3WVWnkI4yOyo', {action: 'signup'})
+        .then(function(token) {
+            $.post("<?php echo $host;?>function/actSignup.php", {
+                fullname: fullname,
+                email: email,
+                password: password
+            }).then(function() {
+                console.debug("SUCCESS");
+            }).catch(function() {
+                console.debug("FAIL");
+            })
+        });
+    });
+}
+</script>
 <div class="login-clean">
-        <form method="post" action="<?php echo $host;?>function/actSignup.php">
+        <form>
             <h2 class="sr-only">Login Form</h2>
             <!-- if signup failed -->
             <?php
@@ -16,9 +39,8 @@ include "header.php";
             <div class="form-group"><input class="form-control" type="text" name="fullname" placeholder="Nama Lengkap"></div>
             <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email"></div>
             <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
-            <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Sign Up</button></div><a class="forgot" href="#">Forgot your email or password?</a></form>
+            <div class="form-group"><button class="btn btn-primary btn-block" onclick="onClickSubmit()" type="submit">Sign Up</button></div><a class="forgot" href="#">Forgot your email or password?</a></form>
     </div>
-    
 <?php
 include "footer.php";
 ?>
