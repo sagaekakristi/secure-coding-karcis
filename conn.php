1fa89fe3420cfaa885de2fc99d23caac17f459c0
Anhar Solehudin <anhsbolic@gmail.com>
2019-09-24 08:48:54 +0700

    set uri alias to secure-coding-karcis

--- conn.php
+++ conn.php
@@ -8,7 +8,7 @@ if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$host = '/karcis/';
+$host = '/secure-coding-karcis/';
 
 error_reporting(E_ALL);
 
